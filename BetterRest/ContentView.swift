5943c4fd61bdef01072de51277846827c6cfb5c8
Rob McBryde <robertmcbryde83@gmail.com>
2025-01-29 10:51:04 +0000

    Date formatting learning

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
         Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             .padding()
             .background(.secondary)
             
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+            .labelsHidden()
+        
+        // same
+        Text(Date.now, format: .dateTime.day().month().year())
+        Text(Date.now.formatted(date: .long, time: .shortened))
+    }
+    
+    func exampleDates() {
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+       
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
