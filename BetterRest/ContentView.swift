5bdceca2264b205c031714faf3176175dc5fa984
Rob McBryde <robertmcbryde83@gmail.com>
2025-01-30 09:43:52 +0000

    Basic start to UI for Better Rest app

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,32 +10,65 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            .padding()
-            .background(.secondary)
-            
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
-        
-        // same
-        Text(Date.now, format: .dateTime.day().month().year())
-        Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+                    .padding()
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount)", value: $coffeeAmount, in: 1...20)
+                    .padding()
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }
     }
     
-    func exampleDates() {
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
-       
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
+    func calculateBedtime() {
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//            .padding()
+//            .background(.secondary)
+//
+//        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+//            .labelsHidden()
+        
+        // same
+//        Text(Date.now, format: .dateTime.day().month().year())
+//        Text(Date.now.formatted(date: .long, time: .shortened))
+
+
+//func exampleDates() {
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+   
+//        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+//        let hour = components.hour ?? 0
+//        let minute = components.minute ?? 0
+
