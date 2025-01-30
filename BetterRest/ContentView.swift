c5f708dca12230d2ce3352295d2809e1e156994b
Rob McBryde <robertmcbryde83@gmail.com>
2025-01-30 10:42:59 +0000

    Improved UI by wrapping it in a form with Vstacks to seperate components also used Swifts inflect markdown syntax to pluralise cup when appropriate

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,34 +9,46 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
-                    .padding()
-                
-                Text("Daily coffee intake (cups)")
-                    .font(.headline)
-                
-                Stepper("\(coffeeAmount)", value: $coffeeAmount, in: 1...20)
-                    .padding()
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake (cups)")
+                        .font(.headline)
+                    
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 0...20)
+                }
             }
             .navigationTitle("BetterRest")
             .toolbar {
